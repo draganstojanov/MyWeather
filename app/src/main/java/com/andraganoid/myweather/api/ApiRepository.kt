5e2db324480d8bf1f1df2d782c29482854d23e5d
Dragan <dragan.stojanov@gmail.com>
2021-04-26 21:29:31 +0200

    Network Check

--- app/src/main/java/com/andraganoid/myweather/api/ApiRepository.kt
+++ app/src/main/java/com/andraganoid/myweather/api/ApiRepository.kt
@@ -1,11 +1,15 @@
 package com.andraganoid.myweather.api
 
+import com.andraganoid.myweather.main.App
 import com.andraganoid.myweather.model.ResponseError
+import com.andraganoid.myweather.model.response.AstronomyResponse
+import com.andraganoid.myweather.model.response.ForecastResponse
 import com.andraganoid.myweather.util.DateFormatter
 import com.andraganoid.myweather.util.EndPoints
 import com.andraganoid.myweather.util.ResponseState
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import retrofit2.Response
 import javax.inject.Inject
 
 
@@ -15,47 +19,63 @@ class ApiRepository @Inject constructor(private val apiService: ApiService) {
 
     suspend fun getAstronomy(query: String): ResponseState {
 
-        val response = apiService.getAstronomy(
-            mapOf(
-                "key" to EndPoints.KEY,
-                "q" to query,
-                "date" to DateFormatter.dateQueryToday(),
+        var response: Response<AstronomyResponse>? = null
+
+        if (App.networkStatus) {
+
+            response = apiService.getAstronomy(
+                mapOf(
+                    "key" to EndPoints.KEY,
+                    "q" to query,
+                    "date" to DateFormatter.dateQueryToday(),
+                )
             )
-        )
-        return try {
-            if (response.isSuccessful) {
-                ResponseState.AstronomyData(response.body())
-            } else {
-                val errorResponse: ResponseError? = Gson().fromJson(response.errorBody()!!.charStream(), type)
-                ResponseState.Error(errorResponse?.error?.message.toString())
+
+            return try {
+                if (response.isSuccessful) {
+                    ResponseState.AstronomyData(response.body())
+                } else {
+                    val errorResponse: ResponseError? = Gson().fromJson(response.errorBody()!!.charStream(), type)
+                    ResponseState.Error(errorResponse?.error?.message.toString())
+                }
+            } catch (exc: Exception) {
+                (ResponseState.Error(exc.localizedMessage!!))
             }
-        } catch (exc: Exception) {
-            (ResponseState.Error(exc.localizedMessage!!))
+        } else {
+            return ResponseState.Error("No network")
         }
     }
 
     suspend fun getForecast(query: String): ResponseState {
-        val response = apiService.getForecast(
-            mapOf(
-                "key" to EndPoints.KEY,
-                "q" to query,
-                "days" to 10,
-                "aqi" to "yes",
-                "alerts" to "no"
+
+        var response: Response<ForecastResponse>? = null
+
+        if (App.networkStatus) {
+            response = apiService.getForecast(
+                mapOf(
+                    "key" to EndPoints.KEY,
+                    "q" to query,
+                    "days" to 10,
+                    "aqi" to "yes",
+                    "alerts" to "no"
+                )
             )
-        )
-        return try {
-            if (response.isSuccessful) {
-                ResponseState.ForecastData(response.body())
-            } else {
-                val errorResponse: ResponseError? = Gson().fromJson(response.errorBody()!!.charStream(), type)
-                ResponseState.Error(errorResponse?.error?.message.toString())
+            return try {
+                if (response.isSuccessful) {
+                    ResponseState.ForecastData(response.body())
+                } else {
+                    val errorResponse: ResponseError? = Gson().fromJson(response.errorBody()!!.charStream(), type)
+                    ResponseState.Error(errorResponse?.error?.message.toString())
+                }
+            } catch (exc: Exception) {
+                (ResponseState.Error(exc.localizedMessage!!))
             }
-        } catch (exc: Exception) {
-            (ResponseState.Error(exc.localizedMessage!!))
+
+
+        } else {
+            return ResponseState.Error("No network")
         }
     }
-
 }
 
 
