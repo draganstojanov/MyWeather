3419ec1d283fc83113512ec2f0266cd7abad2fa4
dragan.stojanov <dragan.stojanov@gmail.com>
2021-09-24 16:58:14 +0200

    network

--- connectivity/src/main/java/com/andraganoid/connectivity/ConnectivityState.kt
+++ connectivity/src/main/java/com/andraganoid/connectivity/ConnectivityState.kt
@@ -9,37 +9,51 @@ import android.os.Build
 
 class ConnectivityState(private val context: Context) {
 
+    private var networkCapabilities: NetworkCapabilities? = null
+    private var connectivityManager: ConnectivityManager? = null
+
     fun setListeners() {
         val networkCallback: ConnectivityManager.NetworkCallback = object : ConnectivityManager.NetworkCallback() {
-
             override fun onAvailable(network: Network) {
                 super.onAvailable(network)
                 connectivityStatus = true
+                networkCapabilities =
+                    connectivityManager?.getNetworkCapabilities(connectivityManager?.activeNetwork)!!
+                if (networkCapabilities != null) {
+                    netTransport = when {
+                        networkCapabilities!!.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> "WIFI"
+                        networkCapabilities!!.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> "CELLULAR"
+                        else -> ""
+                    }
+                }
             }
 
             override fun onUnavailable() {
                 super.onUnavailable()
                 connectivityStatus = false
+                netTransport = ""
             }
 
             override fun onLost(network: Network) {
                 super.onLost(network)
                 connectivityStatus = false
+                netTransport = ""
             }
         }
 
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            connectivityManager.registerDefaultNetworkCallback(networkCallback)
+            connectivityManager?.registerDefaultNetworkCallback(networkCallback)
         } else {
             val request = NetworkRequest.Builder().addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET).build()
-            connectivityManager.registerNetworkCallback(request, networkCallback)
+            connectivityManager?.registerNetworkCallback(request, networkCallback)
         }
     }
 
     companion object {
         var connectivityStatus = false
+        var netTransport: String = ""
     }
 
 }
